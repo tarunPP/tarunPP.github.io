be475ca4073c0da231a15c760d21c9f24ac559ae
Tarun Chakravorty <tarunfzr@gmail.com>
2016-02-22 16:41:57 -0500

    Update sms-hello-monkey.php

--- sms-hello-monkey.php
+++ sms-hello-monkey.php
@@ -1,1 +1,7 @@
-Hellooooooo
+<?php
+    header("content-type: text/xml");
+    echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
+?>
+<Response>
+    <Message>Hello, Mobile Monkey</Message>
+</Response>
